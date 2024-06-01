fd72531bc4a3271a3224618989dd62d5fb9405fa
AdamFariello <ajf270@scarletmail.rutgers.edu>
2024-06-01 04:47:46 -0400

    Added font enum

--- java_functional/Ansi.java
+++ java_functional/Ansi.java
@@ -17,12 +17,13 @@ class Ansi {
     static void print (String s) { System.out.print(s); }
     static void println (String s) { System.out.println(s); }
 
-    
+
+    //interface AnsiText {}
+
     enum Color {
         BLACK("0"), RED("1"), GREEN("2"), YELLOW("3"), BLUE("4"),
-        PURPLE("5"), CYAN("6"), WHITE("7"),
-        TEXT("3"), HIGHLIGHT("4"), BRIGHTTEXT("9"), BRIGHTHIGHLIGHT("10");
-    
+        PURPLE("5"), CYAN("6"), WHITE("7");
+     
         String id;
         Color (String id) {
             this.id = id;
@@ -31,27 +32,49 @@ class Ansi {
         //Required if you want Color.{color} to give back a color
         @Override
         public String toString() {
+            text();
             return id;
         }
-        
+
         public void text() { write("3" + id); }
         public void highlight() { write("4" + id); }
         public void brightText() { write("9" + id); }
         public void brightHighlight() { write("10" + id); }
     }
 
+    enum Font {
+        BOLD("1"), 
+        ITALIC("3"), ITALLIC_OFF("23"), 
+        
+        UNDERLINE("4"), UNDERLINE_OFF("24"), 
+        DOUBLE_UNDERLINE("21"),
+        
+        REVERSE("7"), INVERSE_OFF("27"), 
+        CONCEAL("8"), REVEAL_OFF("28"), 
+        CROSSED_OUT("9"), NOT_CROSSED_OUT("29"), 
+    
+        FRAMED("51"), FRAMED_OFF("54"), 
+        ENCIRCLED("52"), 
+        OVERLINED("53"), OVERLINE_OFF("55");
+        String id;
+        Font (String id) {
+            this.id = id;
+        }
+    
+        @Override
+        public String toString() {
+            write(id);
+            return id;
+        }
+    }
 
-    /*
+    /* 
     class Text implements Ansi{ 
+
         //Inherented functions
         public void write (String s) {
             System.out.print(CSI + s + "m");
              
-        }
-        public void writeRaw(String s) {
-            String tempCSI = "\\u001B" + "[";
-            System.out.print(tempCSI + s + "m"); 
-            
         }
         public void reset () {
             System.out.print(END);
@@ -81,40 +104,9 @@ class Ansi {
             } else {
                 sb.append(Color.void);
             }
-    
-            color = color.toLowerCase();
-            switch (color) {
-                case "black":
-                    sb.append(Color.BLACK);
-                    break;
-                case "red":
-                    sb.append(Color.RED);
-                    break;
-                case "green":
-                    sb.append(Color.GREEN);      
-                    break;
-                case "yellow":
-                    sb.append(Color.YELLOW);     
-                    break;
-                case "blue":
-                    sb.append(Color.BLUE);       
-                    break;
-                case "purple":
-                    sb.append(Color.PURPLE);     
-                    break;
-                case "cyan":
-                    sb.append(Color.CYAN);       
-                    break;
-                case "white":
-                    sb.append(Color.WHITE);      
-                    break;
-                default:
-                    sb.append(Color.BLACK);
-            }
-    
-             write(sb.toString());
+
         }
-        
+       
         public static void color(int color) {  color(color, false); }
         public static void color(int color, boolean isHighlight) {
             if (isHighlight) {
@@ -134,33 +126,6 @@ class Ansi {
                  write("38;2;" + r + ";" + g + ";" + b);
             }
         }
-    
-    
-     
-        //Color manipulate
-        public static void reverse () {  write("7");  } //reverse swaps color of highlight and foreground
-        public static void inverse_off () {  write("27");  }
-        public static void conceal () {  write("8");  }
-        public static void reveal_off () {  write("28");  }
-        public static void crossed_out () {  write("9");  }
-        public static void not_crossed_out () {  write("29");  }
-    
-    
-        //void change
-        public static void bold() {  write("1");  }
-        public static void italic() {  write("3");  } 
-        public static void itallic_off () {  write("23");  }
-        public static void underline () {  write("4");  }
-        public static void underline_off () {  write("24");  }
-        
-    
-        //Underlining void
-        public static void double_underline () {  write("21");  }
-        public static void framed () {  write("51");  }
-        public static void framed_off () {  write("54");  }
-        public static void encircled () {  write("52");  }
-        public static void overlined () {  write("53");  } //Not supported...
-        public static void overline_off () {  write("55");  }
     }
     */
 }
