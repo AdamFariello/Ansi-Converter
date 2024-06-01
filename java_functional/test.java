7b2c3916b977627473a88e0cd47fe209d89e0402
AdamFariello <ajf270@scarletmail.rutgers.edu>
2024-06-01 03:35:56 -0400

    Added work directory for possible Ansi functional solution instead

---
+++ java_functional/test.java
@@ -0,0 +1,9 @@
+package java_functional;
+
+public class test {
+    public static void main (String args []) {
+        Ansi.Color.BLUE.highlight();
+        System.out.print("test");
+        Ansi.resetln();
+    }
+}
