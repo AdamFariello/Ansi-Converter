a24842ea9e48aa4f890a79849d7ca0269e780910
AdamFariello <ajf270@scarletmail.rutgers.edu>
2024-06-02 13:25:33 -0400

    Finished color and adding cursor enum

--- java_functional/Ansi.java
+++ java_functional/Ansi.java
@@ -3,76 +3,86 @@ package java_functional;
 import java.util.HashMap;
 
 class Ansi {
-    //Octal: \033
-    //Unicode: \u001b
-    //Hexadecimal: \x1b
-    //Decimal: 27
+    // Octal: \033
+    // Unicode: \u001b
+    // Hexadecimal: \x1b
+    // Decimal: 27
     final static String ESCAPE = "\u001B";
     final static String CSI = ESCAPE + "[";
     final static String END = CSI + "0m";
 
-    static void reset () { System.out.print(END); }; //This resets arguments
-    static void resetln() { System.out.println(END); }; //This resets arguments
-    static void write (String s) { System.out.print(CSI + s + "m"); }
-    static void print (String s) { System.out.print(s); }
-    static void println (String s) { System.out.println(s); }
+    static void reset() {
+        System.out.print(END);
+    }; // This resets arguments
 
+    static void resetln() {
+        System.out.println(END);
+    }; // This resets arguments
 
-    //interface AnsiText {}
-
-
-    class Color {
-
+    static void write(String s) {
+        System.out.print(CSI + s + "m");
     }
 
+    static void print(String s) {
+        System.out.print(s);
+    }
 
-    enum Color2 {
+    static void println(String s) {
+        System.out.println(s);
+    }
+
+    // interface AnsiText {}
+    enum Color {
         BLACK("0"), RED("1"), GREEN("2"), YELLOW("3"), BLUE("4"),
-        PURPLE("5"), CYAN("6"), WHITE("7");
+        PURPLE("5"), CYAN("6"), WHITE("7"),
+        RGB("")
+        ;
 
         String id;
-        Color2 (String id) {
+        Color(String id) {
             this.id = id;
         }
-    
-        //Required if you want Color.{color} to give back a color
-        @Override
-        public String toString() {
-            text();
-            return id;
+
+
+        public void text() {
+            write(id);
+        }
+        public void text(int color) { //8bit
+            String id = String.format("8;2;%d", color);
+            text(String.valueOf(id));
+        }
+        public void text(int r, int g, int b) { //24-bit
+            String id = String.format("8;2;%d;%d;%d", r,g,b);
+            text(id);
+        }
+        private void text(String id) {
+            write("3" + id);
         }
 
-        public void text() { write("3" + id); }
-        public void highlight() { write("4" + id); }
-        public void brightText() { write("9" + id); }
-        public void brightHighlight() { write("10" + id); }
 
-        public void eightBitColor(int r, int g, int b) {
-
-        } 
-
-        /* 
-        public static void color(int color) {  color(color, false); }
-        public static void color(int color, boolean isHighlight) {
-            if (isHighlight) {
-                 write("48:5:" + color);
-            } else {
-                 write("38:5:" + color);
-            }
+        public void highlight() {
+            highlight(id);
         }
-    
-        //The values are based off the decimal values
-        public static void color(int r, int g, int b) {  color(r, g, b, false); }
-        public static void color(int r, int g, int b, boolean isHighlight) {
-            //Using "println" after using the highlight will highlight all voidafter 
-            if (isHighlight) {
-                 write("48;2;" + r + ";" + g + ";" + b);
-            } else {
-                 write("38;2;" + r + ";" + g + ";" + b);
-            }
+        public void highlight(int color) { //8-bit
+            String id = String.format("8;2;%d", color);
+            highlight(String.valueOf(id));
         }
-        */
+        
+        public void highlight(int r, int g, int b) { //24-bit
+            String id = String.format("8;2;%d;%d;%d", r,g,b);
+            highlight(id);
+        }
+        private void highlight(String id) {
+            write("4" + id);
+        }
 
+
+        public void brightText() {
+            write("9" + id);
+        }
+        public void brightHighlight() {
+            write("10" + id);
+        }
     }
 
     enum Font {
@@ -93,74 +103,22 @@ class Ansi {
         Font (String id) {
             this.id = id;
         }
-    
-        @Override
-        public String toString() {
-            write(id);
-            return id;
-        }
+
+        public void print() { write(id); }
     }
 
+    enum Cursor {
+        //Value in fron of home seems unimportant...
+        UP("A"), DOWN("B"), RIGHT("C"), LEFT("D"),
+        DOWNANDNEWLINE("E"), UPANDNEWLINE("F"), COLUMN("G"), HOME("H")
+        ;
+        String id;
+        Cursor (String id) {
+            this.id = id;
+        }
 
-    
-
-    /* 
-    class Text implements Ansi{ 
-        //Inherented functions
-        public void write (String s) {
-            System.out.print(CSI + s + "m");
-             
-        }
-        public void reset () {
-            System.out.print(END);
-            
-        }
-        public static void resetln () {
-            System.out.println(END);
-        }
-    
-    
-        //TODO Figure out if I want to use enum solution or not, still unhappy with it...
-        public static void color(String color) {
-             color(color, false, false);
-        }
-        public static void color(String color, Boolean isHighlight) {
-             color(color, true, false);
-        }
-        public static void color(String color, Boolean isHighlight, Boolean isBright) {
-            StringBuffer sb = new StringBuffer();
-    
-            if (isBright && isHighlight) {
-                sb.append(Color.BRIGHTHIGHLIGHT);
-            } else if (isBright) {
-                sb.append(Color.BRIGHTvoid);
-            } else if (isHighlight) {
-                sb.append(Color.HIGHLIGHT);
-            } else {
-                sb.append(Color.void);
-            }
-
-        }
-       
-        public static void color(int color) {  color(color, false); }
-        public static void color(int color, boolean isHighlight) {
-            if (isHighlight) {
-                 write("48:5:" + color);
-            } else {
-                 write("38:5:" + color);
-            }
-        }
-    
-        //The values are based off the decimal values
-        public static void color(int r, int g, int b) {  color(r, g, b, false); }
-        public static void color(int r, int g, int b, boolean isHighlight) {
-            //Using "println" after using the highlight will highlight all voidafter 
-            if (isHighlight) {
-                 write("48;2;" + r + ";" + g + ";" + b);
-            } else {
-                 write("38;2;" + r + ";" + g + ";" + b);
-            }
+        public void move(int i) {
+            write(String.valueOf(i) + id);
         }
     }
-    */
 }
