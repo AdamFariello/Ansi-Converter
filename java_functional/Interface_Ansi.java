07948306de8c8538a7895bb05ac749ef4f4c828d
AdamFariello <ajf270@scarletmail.rutgers.edu>
2024-06-04 16:32:50 -0400

    Added Raw ansi variables and methods for debugging

--- java_functional/Interface_Ansi.java
+++ java_functional/Interface_Ansi.java
@@ -8,4 +8,9 @@ public interface Interface_Ansi {
     final static String ESCAPE = "\u001B";
     final static String CSI = ESCAPE + "[";
     final static String END = CSI + "0m";
+
+
+    final static String ESCAPE_raw = "\\u001B";
+    final static String CSI_raw = ESCAPE_raw + "[";
+    final static String END_raw = CSI_raw + "0m";
 }
