ed0b7d3d2d59fec759ca6111d640e863c9081fc3
AdamFariello <ajf270@scarletmail.rutgers.edu>
2024-06-10 17:47:48 -0400

    Changed name of fileHandler to FileHandler, and also change its invoke in ScriptHandler.java

--- ansiUtil/FileHandler.java
+++ ansiUtil/FileHandler.java
@@ -5,7 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class fileHandler {
+public class FileHandler {
     public static String readSingleLineFile(String fileNameAndLocation) {
         if (! doesFileExist(fileNameAndLocation))
             return null;
